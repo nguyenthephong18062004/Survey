65dd38da198d2703d14537bcc1ccf7f1d83d492d
nguyenthephong18062004 <nguyenthephong18062004@gmail.com>
2026-06-14 11:26:27 +0700

    mfix lỗi port

--- server/db.js
+++ server/db.js
@@ -1,24 +1,31 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+
+// Luôn luôn nạp dotenv để chạy dưới local ngon lành
 dotenv.config();
 
 const pool = mysql.createPool({
+  // Nếu chạy trên Render, nó sẽ lấy DB_HOST từ Render (thomas.proxy.rlwy.net)
+  // Nếu chạy ở local, nó sẽ lấy DB_HOST từ file .env local của bạn (127.0.0.1 hoặc localhost)
   host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT) || 3306, 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-
 });
+
+// Kiểm tra kết nối
 pool.getConnection()
   .then(conn => {
-    console.log("==> ĐÃ KẾT NỐI DATABASE RAILWAY THÀNH CÔNG! 🎉");
+    console.log(`==> ĐÃ KẾT NỐI DATABASE THÀNH CÔNG TỚI HOST: ${process.env.DB_HOST} 🎉`);
     conn.release();
   })
   .catch(err => {
-    console.error("❌ Lỗi kết nối DB chi tiết tại đây:", err.message);
+    console.error("❌ Lỗi kết nối Database chi tiết:", err.message);
   });
+
 export default pool;
